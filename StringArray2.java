b4192b22b562794bcad8448e20e58597a8d60536
santosh udavant <128016355+santoshudavant123@users.noreply.github.com>
2023-11-16 21:04:11 +0530

    Add files via upload

---
+++ StringArray2.java
@@ -0,0 +1,18 @@
+package day2;
+//wap to ask 5 names from user and store them in an array
+import java.util.*;
+public class StringArray2 {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		
+		String g= new String("abc");//g is assigned value =abc
+		
+		String h[]= new String[5]; // h is declared as array of size 5
+		h[0]= "amit";
+		h[1]= "neha";
+		h[2]= new String("gagan");
+        // h[3]= s.next();
+	}
+
+}
